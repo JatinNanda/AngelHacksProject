82c2908b6583f11ab2da5c93a2726d61d690ad43
AdvaithE <advaithv@gatech.eduS>
2015-05-30 17:39:18 +0200

    Added reading from local countries file to mainActivity

--- app/src/main/java/com/example/jatin1/angelhacksproject/MainActivity.java
+++ app/src/main/java/com/example/jatin1/angelhacksproject/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -39,5 +42,30 @@ public class MainActivity extends ActionBarActivity {
 
     public boolean isWorking() {
         return true;
+    }
+
+    public String loadJSONFromAsset() {
+        String json = null;
+        try {
+
+            InputStream is = getAssets().open("yourfilename.json");
+
+            int size = is.available();
+
+            byte[] buffer = new byte[size];
+
+            is.read(buffer);
+
+            is.close();
+
+            json = new String(buffer, "UTF-8");
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+
     }
 }
