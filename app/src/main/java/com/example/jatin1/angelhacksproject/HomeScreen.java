7449fd99bcaa9b1914bd6b4b7ada2d5bccd7ddaf
AdvaithE <advaithv@gatech.eduS>
2015-05-30 22:16:50 +0200

    Merge branch 'Jatin'sBranch' of https://github.com/JatinNanda/AngelHacksProject
    Conflicts:
    	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/jatin1/angelhacksproject/HomeScreen.java
+++ app/src/main/java/com/example/jatin1/angelhacksproject/HomeScreen.java
@@ -1,43 +1,21 @@
 package com.example.jatin1.angelhacksproject;
 
-import android.content.Intent;
-import android.content.res.AssetManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONTokener;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.HashMap;
 
 
 public class HomeScreen extends ActionBarActivity {
 
-    private static final String QUERY_URL = "https://drive.google.com/file/d/0B2m3M7EK8AhNV1pPeFZRM202LWs/view?usp=sharing";
-    Button country;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
     }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
